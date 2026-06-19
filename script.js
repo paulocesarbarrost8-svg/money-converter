8627e4f40fe9e4c02eb9c65f9ae7db100ac5c59a
Paulo <paulocesarbarrost8@gmail.com>
2026-06-19 10:39:12 -0300

    novasMoedas

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function clicar() {
     const valor = Number(meuInput.value)
     const dolar = 5.10;
     const euro = 5.91;
+    const libra = 6.81
+    const bitcoin = 322.916
 
     if (meuInput.value === "" || valor <= 0) { //se o select estiver sem nenhum valor, entre aqui
         resultado.innerHTML = "digite um valor válido"
@@ -21,15 +23,18 @@ function clicar() {
     if (moedaSelect.value.includes("Dólar")) { //se o select estiver selecionando o valor de dólar, entre aqui
         const valorConvertido = valor / dolar;
         resultado.innerHTML = "$ " + valorConvertido.toFixed(2)
-        currencyImage.src = "./assts/dolar.png"
         chrrencmeney.innerHTML = "Dólar Americano"
 
     } else if (moedaSelect.value.includes("Euro")) { //se o select estiver selecionando o valor de euro, entre aqui
         const valorConvertido = valor / euro;
         resultado.innerHTML = "€ " + valorConvertido.toFixed(2)
-        currencyImage.src = "./assts/euro.png"
-        currencyImage.style.width = "55px";
         chrrencmeney.innerHTML = "Euro"
+    } else if (moedaSelect.value.includes("Libra")){
+        const valorConvertido = valor / libra;
+        resultado.innerHTML = "£" + valorConvertido.toFixed(2)
+    } else if(moedaSelect.value.includes("Bitcoin")){
+        const valorConvertido = valor / bitcoin;
+        resultado.innerHTML = "₿" + valorConvertido.toFixed(1)
     }
 }
 
@@ -41,6 +46,14 @@ function changeCurrency(){
         currencyImage.src = "./assts/euro.png"
         currencyImage.style.width = "55px";
         chrrencmeney.innerHTML = "Euro"
+    } else if(moedaSelect.value.includes("Libra")){
+        currencyImage.src = "./assts/libra.png"
+        currencyImage.style.width = "50px";
+        chrrencmeney.innerHTML = "Libra"
+    } else if(moedaSelect.value.includes("Bitcoin")){
+        currencyImage.src = "./assts/bitcoin .png"
+        currencyImage.style.width = "50px";
+        chrrencmeney.innerHTML = "bitcoin"
     }
 
     clicar()
